92cc6769fe6355c36cebdf60f1f1c68ee475ff52
gmrdelatorre <gmrdt7@gmail.com>
2014-04-29 14:16:59 +0800

    Added content and HTML templates

---
+++ DCS_Website/contents/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from contents.models import Announcement, Event, Article
+
+# Register your models here.
+class AnnouncementAdmin(admin.ModelAdmin):
+	list_display = ('title', 'pub_date')
+	list_filter = ['pub_date']
+	search_fields = ['title']
+
+class EventAdmin(admin.ModelAdmin):
+	list_display = ('description', 'date')
+	list_filter = ['date']
+	search_fields = ['description']
+
+class NewsAdmin(admin.ModelAdmin):
+	list_display = ('title', 'pub_date')
+	list_filter = ['pub_date']
+	search_fields = ['title']
+
+admin.site.register(Announcement, AnnouncementAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Article, NewsAdmin)
